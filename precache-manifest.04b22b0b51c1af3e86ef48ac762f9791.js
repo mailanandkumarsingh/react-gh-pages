92cecf576268c78d373e1ebae91845658e7713e7
Singh Anand Kumar <a08412@MACLA200571.local>
2018-10-08 10:25:57 +0100

    Updates

---
+++ precache-manifest.04b22b0b51c1af3e86ef48ac762f9791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "0dbc055df0e112437506",
+    "url": "/react-gh-pages/static/js/main.0dbc055d.chunk.js"
+  },
+  {
+    "revision": "fc0f1b9c7e2b8cba5808",
+    "url": "/react-gh-pages/static/js/1.fc0f1b9c.chunk.js"
+  },
+  {
+    "revision": "0dbc055df0e112437506",
+    "url": "/react-gh-pages/static/css/main.05a7a141.chunk.css"
+  },
+  {
+    "revision": "1bce3237e1791555041eb0e685ad389e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
